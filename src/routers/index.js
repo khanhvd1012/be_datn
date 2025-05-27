143a96e14a65256922ce93c88bf106347b7e1e67
khanhvd1012 <khanhvdph49723@gmail.com>
2025-05-27 13:29:22 +0700

    thêm productRoute

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import brandRouter from "./brand_RT";
 import categoryRouter from "./category_RT";
+import productRoute from "./product_RT";
 
 const router = Router();
 
 router.use("/brands", brandRouter);
 router.use("/categorys", categoryRouter);
+router.use("/products",productRoute);
 
 export default router;
