de915c103396b1212062c76b3dbc045d20f6d43b
Jnag <giangnguyen3092005@gmail.com>
2025-09-21 14:38:11 +0700

    fix

--- src/models/size_MD.js
+++ src/models/size_MD.js
@@ -4,11 +4,12 @@ const sizeSchema = new mongoose.Schema({
     size: {
         type: Number,
         required: true,
-        unique: true,
+        unique: true, 
     },
 }, {
     timestamps: true
 });
+
 const Size = mongoose.models.Size || mongoose.model('Sizes', sizeSchema);
 
 export default Size;
